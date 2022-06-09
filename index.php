7f908c3d1c7da95650d2318756ab4aa6571070ff
joelchoi85 <jymania@naver.com>
2022-06-09 14:45:21 +0900

    ..

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>공부는 끝이 없나보다</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+echo "<a href='./html' class='btn btn-primary'>HTML</a>";
+echo "<a href='./php' class='btn btn-secondary'>PHP</a>";
+?>
+
